cbe41adf14afe2d2817c1fa7cec6bd594c21b2e3
João Faria <Joao.Faria@astro.up.pt>
2013-10-09 20:09:18 +0100

    likelihood calculation working?

--- likelihood.f90
+++ likelihood.f90
@@ -1,10 +1,13 @@
 !*  Purpose
 !*  =======
-!*  Currently just a wrapper aroung get_rv for testing
-subroutine test_wrapper(time, P, K, ecc, omega, t0, vsys, vel, nt, np)
+!*  Likelihood calculation
+subroutine likelihood(time, datum, sigma, &
+                      P, K, ecc, omega, t0, vsys, vel, nt, np)
     implicit none
 
 !f2py intent(in) time(nt)
+!f2py intent(in) datum(nt)
+!f2py intent(in) sigma(nt)
 !f2py intent(inout) vel(nt)
 !f2py intent(in) P(np)
 !f2py intent(in) K(np)
@@ -17,11 +20,28 @@ subroutine test_wrapper(time, P, K, ecc, omega, t0, vsys, vel, nt, np)
 
 ! Input arguments
     integer nt, np ! number of observations, planets
-    real (kind=8), dimension(nt) :: time, vel
+    real (kind=8), dimension(nt) :: time, datum, sigma, vel
     real (kind=8), dimension(np) :: p, k, ecc, omega, t0
     real (kind=8) :: vsys
 
+! Local variables
+    integer,parameter :: sp = selected_real_kind(p=6,r=37)
+    integer,parameter :: dp = selected_real_kind(p=15,r=307)
 
+    real(dp), parameter :: pi = 3.1415926535897932384626433832795029_dp
+    real(dp), parameter :: twopi = 2.0_dp * pi 
+
+    real(dp) :: lhd     
+
+
+
+    ! get the radial velocity model with these parameters (in vel)
     call get_rvN(time, P, K, ecc, omega, t0, vsys, vel, nt, np)
+
+    write(*,*) vel(1)
+
+    lhd = product((1._dp / sqrt(twopi*sigma**2)) * exp(-0.5_dp * ((datum - vel)/sigma)**2))
+    write(*,*) lhd * 1d9
+
 
 end subroutine
