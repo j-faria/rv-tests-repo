a72d7c65f76662d9cdb2ab0195fa6b356f713ef9
João Faria <Joao.Faria@astro.up.pt>
2013-10-09 15:49:40 +0100

    testing

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 F2PYFLAGS = --f77exec="/home/joao/mesasdk/bin/gfortran" --f77flags="-O3 -Ofast"
 
 all: get_rv1.so get_rvN.so
-	
+
+test: get_rv1.so get_rvN.so
+	python test.py
+	ndiff test/out_test.txt test/out_normal.txt
+
 get_rv1.so: get_rv1.f90
 	f2py -m get_rv1 -c $^ $(F2PYFLAGS)
 
---
+++ test.py
@@ -0,0 +1,14 @@
+from get_rv1 import get_rv 		# 1 planet
+from get_rvN import get_rvn 	# n planets
+
+from numpy import *
+
+
+times = linspace(2449460, 2452860, 100)
+vel1 = zeros_like(times)
+vel2 = zeros_like(times)
+
+get_rv(times, 1425, 10, 0.9, 0.2, 2452000, vel1)
+get_rvn(times, [1425, 13], [10, 3], [0.9, 0.02], [0.2, 0.3], [2452000, 2451000], vel2)
+
+savetxt('test/out_test.txt', zip(times, vel1, vel2))
